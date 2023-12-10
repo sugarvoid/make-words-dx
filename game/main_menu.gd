bbdf24f3269871fd7674cfb017543f16948785c6
sugarvoid <gamedev@kitargo.com>
2023-12-10 08:38:53 -0600

    Add main menu

---
+++ game/main_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _start_game() -> void:
+	get_tree().change_scene_to_file("res://game/game.tscn")
+
+func _input(event) -> void:
+	if event.is_action_pressed("space"):
+		_start_game()
