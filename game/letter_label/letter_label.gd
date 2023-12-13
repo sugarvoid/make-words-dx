ae469533b966a30cb970605a7e8667e9bd84ed64
Dave <gamedev@kitargo.com>
2023-12-13 07:30:38 -0600

    Add shake to letter class

--- game/letter_label/letter_label.gd
+++ game/letter_label/letter_label.gd
@@ -25,10 +25,35 @@ func fade_away() -> void:
 func move_to_pos(pos: Vector2) -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "global_position", pos, 0.6)
+
+
+func shake() -> void:
+	var shake = 5
+	var shake_amount = 2
+	var shake_duration = 0.1
+	var shake_count = 3
+	var tween = get_tree().create_tween()
 	
+	var start_pos: Vector2 = self.position
+	
+	
+
+	for i in shake_count:
+		randomize()
+		#var x_ran = start_pos.x + randf_range((start_pos.x - shake_amount), (start_pos.x + shake_amount))
+		#var y_ran = start_pos.y + randf_range((start_pos.y - shake_amount), (start_pos.y + shake_amount))
+		var x_ran = randf_range((start_pos.x - shake_amount), (start_pos.x + shake_amount))
+		var y_ran = randf_range((start_pos.y - shake_amount), (start_pos.y + shake_amount))
+		tween.tween_property(self, "position", Vector2(x_ran, y_ran), shake_duration)
+		tween.tween_property(self, "position", start_pos, shake_duration)
+
 
 func flash_wrong() -> void:
 	change_color(Color("4c4139"))
 
 func reset_color() -> void:
 	change_color(Color("e5dacd"))
+
+
+func _on_timer_timeout():
+	shake()
