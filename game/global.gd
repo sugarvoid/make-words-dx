908b4e5e7713d4162704eb98476c946e5abf8697
Dave <gamedev@kitargo.com>
2024-03-07 08:46:03 -0600

    Start on global file

---
+++ game/global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const GAME_HISTORY_PATH: String = "user://game_history.json"
+const USED_WORDS_PATH: String = "user://used_words.txt"
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
